4d02f5f0a5531b5a6357312dfdb416bdb7bb8f81
TBH9012 <tranbinhhoa999@gmail.com>
2025-01-06 17:58:23 +0700

    Add files via upload

---
+++ HeThongQuanLyBanHang/Class/KetNoidatabase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Drawing;
+using HeThongQuanLyBanHang.Class;
+using System.Data.Entity;
+using System.Runtime.Remoting.Contexts;
+
+namespace HeThongQuanLyBanHang.Class
+{
+    internal class KetNoidatabase
+    {
+        private static Model1 Con;  // Khai báo đối tượng DbContext
+
+        public static void Connect()
+        {
+            try
+            {
+                Con = new Model1();  // Khởi tạo DbContext
+                MessageBox.Show("Kết nối thành công");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Không thể kết nối với cơ sở dữ liệu: " + ex.Message);
+            }
+        }
+
+        public static void Disconnect()
+        {
+            if (Con != null)
+            {
+                Con.Dispose();  // Đóng DbContext
+            }
+        }
+      
+    }
+}
